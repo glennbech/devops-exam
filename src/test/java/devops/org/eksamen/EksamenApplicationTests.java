14d5913ad2d45d4020c7376c662c8ffebd6d0e34
guberArmin <gubarm18@student.kristiania.no>
2020-11-14 00:15:38 +0100

    Edited test to fail, so that I can check if travis build fails

--- src/test/java/devops/org/eksamen/EksamenApplicationTests.java
+++ src/test/java/devops/org/eksamen/EksamenApplicationTests.java
@@ -93,7 +93,7 @@ class EksamenApplicationTests {
         given()
                 .get("imgs/1")
                 .then()
-                .statusCode(200);
+                .statusCode(303);
     }
 
 }
